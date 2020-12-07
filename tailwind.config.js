2262b752368eb76c46062edcd457679806ed8f91
David Daza <03dazal@gmail.com>
2020-12-07 14:09:47 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'space-cadet': '#202047',
+        caribean: '#1cdbaa',
+        dodger: '#1688fb',
+        fuchsia: '#ff33eb',
+        lavender: '#ffebef',
+        onyx: '#212135',
+        orange: '#f6650f',
+        ryb: '#fb361d',
+      },
+      fontFamily: {
+        usual: ['usual', 'sans-serif'],
+        lores: ['lores-9-plus-narrow', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
